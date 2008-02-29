e1bdb8ee7aad3e2c0c1708e720f09eae687db066
Antoine Toulme <atoulme@intalio.com>
2008-02-29 15:08:57 +0000

    added new sample, to extend the edit part factory

---
+++ samples/org.eclipse.stp.bpmn.sample.newEditPartProvider/src/org/eclipse/stp/bpmn/sample/neweditpartprovider/NewEditPartProviderPlugin.java
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * Copyright (c) 2008, Intalio Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Intalio Inc. - initial API and implementation
+ *******************************************************************************
+ * Date         Author                  Changes
+ * Feb 29, 2008  Antoine Toulme      Created
+ */
+package org.eclipse.stp.bpmn.sample.neweditpartprovider;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class NewEditPartProviderPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.stp.bpmn.sample.newEditPartProvider";
+
+	// The shared instance
+	private static NewEditPartProviderPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public NewEditPartProviderPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static NewEditPartProviderPlugin getDefault() {
+		return plugin;
+	}
+
+}
