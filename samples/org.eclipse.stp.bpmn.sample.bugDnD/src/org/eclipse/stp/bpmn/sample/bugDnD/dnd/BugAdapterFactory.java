221192fc67fa40ce5b733672a50e6e85324533b6
Antoine Toulme <atoulme@intalio.com>
2008-03-07 22:13:51 +0000

    added the sample for eclipsecon2008 for bug DnD

---
+++ samples/org.eclipse.stp.bpmn.sample.bugDnD/src/org/eclipse/stp/bpmn/sample/bugDnD/dnd/BugAdapterFactory.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * Copyright (c) 2008, Intalio Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Intalio Inc. - initial API and implementation
+ *******************************************************************************
+ * Date			Author					Changes
+ * Mar 6, 2008	Antoine Toulme		Created
+ */
+package org.eclipse.stp.bpmn.sample.bugDnD.dnd;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.stp.bpmn.dnd.IDnDHandler;
+import org.eclipse.stp.bpmn.sample.bugView.bug.view.IBug;
+
+/**
+ * The adapter factory that creates a handler to drop bugs on the diagram.
+ *
+ * @author <a href="http://www.intalio.com">Intalio Inc.</a>
+ * @author <a href="mailto:atoulme@intalio.com">Antoine Toulme</a>
+ */
+public class BugAdapterFactory implements IAdapterFactory {
+
+    public Object getAdapter(Object adaptableObject, Class adapterType) {
+        if (adaptableObject instanceof IBug && 
+                IDnDHandler.class.equals(adapterType)) {
+            return new BugDnDHandler((IBug) adaptableObject);
+        }
+        return null;
+    }
+
+    public Class[] getAdapterList() {
+        return new Class[] {IDnDHandler.class};
+    }
+
+}
